81fa62049dd3ab64e73a0bee2066216c98f47689
Daniel Coady <me@pondo.dev>
2022-04-16 19:22:56 +1000

    initial commit

---
+++ deploy_config_template.py
@@ -0,0 +1,7 @@
+#!/opt/homebrew/bin/python3
+
+if __name__ == "__main__": raise Exception("should not run config file")
+
+PUBLIC_DOMAIN = "example.com"
+SERVER_USER = "admin"
+SERVER_CONTENT_DIR = "/home/admin/serve/content"
